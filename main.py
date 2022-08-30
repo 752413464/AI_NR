9c87a732c60b558d73acea9bc2e9ce93ff456674
Huii <72867414+752413464@users.noreply.github.com>
2022-08-30 10:34:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,116 @@
 import os
-from xml.etree.ElementInclude import default_loader
 import torch
 import argparse
 from torch.backends import cudnn
-from net.Unet import Unet
-from net.UNetplusplus import UNetplusplus
-from net.U2Net import U2NETP
-from net.PMRID import PMRID
-from net.PMRID_ca import PMRID_ca
-from net.PMRID_caplus import PMRID_caplus
-from net.PMRID_caplusp import PMRID_caplusp
-from net.MPRNet import MPRNet
-from train import _train
-from test import _test
 
+
+from model.MPRNet import MPRNet
+from model.PMRID import PMRID
+from model.PMRID_caplusp import PMRID_caplusp
+from model.PMRID_up import PMRID_up
+from model.cycleisp import isp
+
+from train_zte import _train_zte
+from test_zte import _test_zte
+
+from train_sony import _train_sony
+from test_sony import _test_sony
+
+from train_reno import _train_reno
+from test_reno import _test_reno
+
+from train_sidd import _train_sidd
+from test_sidd import _test_sidd
 
 
 def main(args):
-    # CUDNN
-    # cudnn.benchmark = True
+
+    net_list = {
+        'PMRID': PMRID(),
+        'MPR': MPRNet(),
+        'PMRID_up':PMRID_up(),
+        'PMRID_caplusp':PMRID_caplusp(),
+        'isp':isp()
+    }
+ 
+    net = net_list[args.model_name]
+
+    model = net.to(args.device)
 
     if not os.path.exists(args.model_save_dir):
         os.makedirs(args.model_save_dir)
-    if not os.path.exists(args.result_dir):
-        os.makedirs(args.result_dir)
-    
-    model = PMRID_caplusp().to(args.device)
-    torch.cuda.empty_cache()
-    
-    if args.mode == 'train':
-       _train(model, args) # for img_split
-       #_train(model, args) # for img_total
 
-    elif args.mode == 'test':
-        _test(model, args)
+
+    torch.cuda.empty_cache()
+
+    if args.dataset == 'zte':
+
+        if not os.path.exists(args.result_dir):
+                    os.makedirs(args.result_dir)  
+
+        if args.mode == 'train':
+            _train_zte(model, args) 
+        else:
+            _test_zte(model, args)
+
+    elif args.dataset =='sony':
+        
+        if not os.path.exists(args.result_dir):
+                    os.makedirs(args.result_dir)
+
+        if args.mode == 'train':
+            _train_sony(model, args) 
+        else:
+            _test_sony(model, args)
+          
+    elif args.dataset =='reno':
+        
+        if not os.path.exists(args.result_dir):
+                    os.makedirs(args.result_dir)
+
+        if args.mode == 'train':
+            _train_reno(model, args) 
+        else:
+            _test_reno(model, args)
+              
+    elif args.dataset =='sidd':
+        
+        if not os.path.exists(args.result_dir):
+                    os.makedirs(args.result_dir)
+
+        if args.mode == 'train':
+            _train_sidd(model, args) 
+        else:
+            _test_sidd(model, args)
+
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
 
+    parser = argparse.ArgumentParser()
     # Directories
-    parser.add_argument('--model_name', default='PMRID_caplusp',  type=str)
-    parser.add_argument('--data_dir', type=str, default='data/')
+    parser.add_argument('--model_name', default='isp',  type=str)
     parser.add_argument('--mode', default='train', choices=['train', 'test'], type=str)
+    parser.add_argument('--dataset', default='sidd', choices=['sony' , 'zte' , 'reno'], type = str)
 
     # Train
+    parser.add_argument('--data_dir', type=str, default='data/')
     parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--learning_rate', type=float, default=1e-4) # default=1e-4
+    parser.add_argument('--learning_rate', type=float, default=6e-4) # default=1e-4
     parser.add_argument('--weight_decay', type=float, default=0) ## default=0, 5e-2
-    parser.add_argument('--num_epoch', type=int, default=999)
-    parser.add_argument('--device', type=str, default = 'cuda:7' )
-    # parser.add_argument('--print_freq', type=int, default=1)
-    parser.add_argument('--num_worker', type=int, default=2)
-    # parser.add_argument('--save_freq', type=int, default=10)
-    # parser.add_argument('--valid_freq', type=int, default=10)
-    # parser.add_argument('--ckp', type=str, default='checkpoints/best_model.pth') #'../checkpoints/pretrained/pretrained.pth')
-    # parser.add_argument('--resume', type=str, default='')
+    parser.add_argument('--num_epoch', type=int, default=50)
+    parser.add_argument('--device', type=str, default = 'cuda:0')
+    parser.add_argument('--num_worker', type=int, default=0)
     parser.add_argument('--gamma', type=float, default=0.5)
-    parser.add_argument('--lr_steps', type=list, default=[(x+2) * 40 for x in range(999//50)])
-    parser.add_argument('--test_dir', type=str, default='data/valid/noisy/')
-
+    parser.add_argument('--test_dir', type=str, default='data/valid/')
     args = parser.parse_args()
+
     args.model_save_dir = os.path.join('demo_code/checkpoints/', args.model_name)
-    args.ckp = os.path.join(args.model_save_dir,'best_model.pth')
+    args.ckp = os.path.join(args.model_save_dir, args.dataset + '.pth')
+    # args.ckp = os.path.join(args.model_save_dir,'zte.pth')
+    # args.ckp = ('/mnt/AI denoise/weights/MPRNet_best_64_L1_aug.pth')
 
+    args.result_dir = os.path.join('demo_code/results/', args.model_name, str(args.dataset) + '_image')
 
-    args.result_dir = os.path.join('demo_code/results/', args.model_name, 'infer_image')
-    args.result_dir_rgb = os.path.join('demo_code/results/', args.model_name, 'infer_image_rgb')
     main(args)
 
